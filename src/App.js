8ccf259b3eab5b57b9afd501f03bf2d47c6e7f73
archivizer.workos <archivizer.workos@gmail.com>
2021-02-04 17:59:28 +0200

    Preparing place for main content & add header, footer components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+// Используя React и сервис для тестового REST api https://jsonplaceholder.typicode.com/ - вывести список пользователей. В этом списке должны быть
+// Имя, Фамилия, емейл и полный адрес(одной строкой).
+// По нажатию на пользователя нужно показать его посты (доступны по запросу /post) и все комментарии к посту (доступны по запросу /comments)
+// А так же вывести все его альбомы доступные по запросу (/albums) и под каждым альбомом вывести слайдер с фотографиями (/photos). Стили и тд - на ваше усмотрение.
+import React from "react";
 import './App.css';
+import { Container } from "react-bootstrap";
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Header />
+        <main>
+          <Container>
+            <h1>Main content must be here</h1>
+          </Container>
+        </main>
+      <Footer />
+    </>
   );
 }
 
 export default App;
+
+
