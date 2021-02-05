fc7d1c973207091beff4a1401cec1e346266ea9b
archivizer.workos <archivizer.workos@gmail.com>
2021-02-05 13:49:10 +0200

    Delete task

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-// Используя React и сервис для тестового REST api https://jsonplaceholder.typicode.com/ - вывести список пользователей. В этом списке должны быть
-// Имя, Фамилия, емейл и полный адрес(одной строкой).
-// По нажатию на пользователя нужно показать его посты (доступны по запросу /post) и все комментарии к посту (доступны по запросу /comments)
-// А так же вывести все его альбомы доступные по запросу (/albums) и под каждым альбомом вывести слайдер с фотографиями (/photos). Стили и тд - на ваше усмотрение.
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
