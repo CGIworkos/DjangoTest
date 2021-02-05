eeb15c0b9801d24cf7d98b72a747dc44a2894b62
archivizer.workos <archivizer.workos@gmail.com>
2021-02-05 11:26:21 +0200

    Implementing router path for name Id

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@
 // По нажатию на пользователя нужно показать его посты (доступны по запросу /post) и все комментарии к посту (доступны по запросу /comments)
 // А так же вывести все его альбомы доступные по запросу (/albums) и под каждым альбомом вывести слайдер с фотографиями (/photos). Стили и тд - на ваше усмотрение.
 import React from "react";
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import UsersList from "./components/UsersList";
+import User from "./components/User";
 
 const App = () => {
   return (
-      <>
+      <Router>
         <Header />
         <main>
           <Container>
-            <UsersList />
+            <Route path='/' component={UsersList} exact/>
+            <Route path='/user/:id' component={User} />
           </Container>
         </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
